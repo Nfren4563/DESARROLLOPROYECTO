4a52d5b3109247c03adbb937eb6e9e8d63279065
Nfren4563 <a224200249@unison.mx>
2025-12-06 17:58:57 -0700

    Update ClienteMain.java
    SE INICIA SERVIDOR EN MAIN CLIENTE

--- cliente/src/main/java/mx/unison/cliente/main/ClienteMain.java
+++ cliente/src/main/java/mx/unison/cliente/main/ClienteMain.java
@@ -1,9 +1,23 @@
 package mx.unison.cliente.main;
 
+import mx.unison.servidor.conexion.servidor;
+import mx.unison.cliente.main.appFrame;
 import javax.swing.*;
 
 public class ClienteMain {
     public static void main(String[] args) {
+
+        new Thread(() -> {
+            servidor s = new servidor(5000);
+            s.start();
+        }).start();
+        
+        try {
+            Thread.sleep(1000); 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(() -> {
             new appFrame().setVisible(true);
         });
